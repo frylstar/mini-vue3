aa28d58b33a7c00faec184eb9ccbad05e2fd4653
frylstar <frylstar@gmail.com>
2024-03-19 23:28:25 +0800

    feat: effect & reactive & 依赖收集/触发依赖

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,54 @@
+class ReactiveEffect {
+    private _fn: any;
+
+    constructor(fn) {
+        this._fn = fn
+    }
+
+    run() {
+        activeEffect = this
+        this._fn()
+    }
+}
+
+const targetMap = new Map()
+export function track(target, key) {
+    // target -> key -> dep
+    let depsMap = targetMap.get(target)
+    // 初始化的时候，如果depsMap没有的话，创建一个
+    if (!depsMap) {
+        depsMap = new Map()
+        targetMap.set(target, depsMap)
+    }
+
+    let dep = depsMap.get(key)
+    if (!dep) {
+        // dep是Set ?
+        dep = new Set()
+        depsMap.set(key, dep)
+    }
+
+    // 这里需要拿到fn，那么我们应该如何拿到fn呢？
+    // 可以利用一个全局变量去获取
+    dep.add(activeEffect)
+}
+
+export function trigger(target, key) {
+    // 把之前key对应的dep依赖取出来
+    let depsMap = targetMap.get(target)
+
+    let dep = depsMap.get(key)
+
+    for (const effect of dep) {
+        effect.run()
+    }
+}
+
+
+let activeEffect;
+
+export function effect(fn) {
+    const _effect = new ReactiveEffect(fn)
+
+    _effect.run()
+}
