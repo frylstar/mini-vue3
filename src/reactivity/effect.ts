f79f28b7ba01b503065e2b0201300251627a4387
frylstar <frylstar@gmail.com>
2024-03-20 23:07:05 +0800

    feat: effect返回runner

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -7,7 +7,7 @@ class ReactiveEffect {
 
     run() {
         activeEffect = this
-        this._fn()
+        return this._fn()
     }
 }
 
@@ -51,4 +51,6 @@ export function effect(fn) {
     const _effect = new ReactiveEffect(fn)
 
     _effect.run()
+
+    return _effect.run.bind(_effect)
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -18,4 +18,19 @@ describe('effect', () => {
         user.age++
         expect(nextAge).toBe(12)
     })
+
+    // 为啥这里需要返回runner函数呢？
+    it('should return runner when call effect', () => {
+        // 1. effect(fn) -> function(runner) -> fn -> return fn的返回值
+        let foo = 10
+        const runner = effect(() => {
+            foo++
+            return "foo"
+        })
+
+        expect(foo).toBe(11)
+        const r = runner()
+        expect(foo).toBe(12)
+        expect(r).toBe('foo')
+    })
 })
