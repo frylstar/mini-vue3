b7b80f76f1c53e54a0e6ab72f20b91b0186725ab
frylstar <frylstar@gmail.com>
2024-03-21 23:46:38 +0800

    feat: 实现effect的stop｜onStop功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,14 +1,36 @@
+import { extend } from "../shared";
 class ReactiveEffect {
     private _fn: any;
+    deps = [];
+    active = true;
+    onStop?: Function | null;
+    public scheduler: Function | undefined;
 
-    constructor(fn, public scheduler?) {
+    constructor(fn, scheduler?: Function) {
         this._fn = fn
+        this.scheduler = scheduler
     }
 
     run() {
         activeEffect = this
         return this._fn()
     }
+
+    stop() {
+        if (this.active) {
+            cleanupEffect(this)
+            if (this.onStop) {
+                this.onStop()
+            }
+            this.active = false
+        }
+    }
+}
+
+function cleanupEffect(effect) {
+    effect.deps.forEach((dep: any) => {
+        dep.delete(effect)
+    })
 }
 
 const targetMap = new Map()
@@ -28,9 +50,12 @@ export function track(target, key) {
         depsMap.set(key, dep)
     }
 
+    if (!activeEffect) return
     // 这里需要拿到fn，那么我们应该如何拿到fn呢？
     // 可以利用一个全局变量去获取
     dep.add(activeEffect)
+    // 在实例中存储dep，(当没有调用effect的时候，activeEffect为undefined，需要加个判断
+    activeEffect.deps.push(dep)
 }
 
 export function trigger(target, key) {
@@ -53,10 +78,21 @@ let activeEffect;
 
 export function effect(fn, options: any = {}) {
     const _effect = new ReactiveEffect(fn, options.scheduler)
+    // options
+    // _effect.onStop = options.onStop
+    // Object.assign(_effect, options)
+    extend(_effect, options)
 
     _effect.run()
 
-    const runner = _effect.run.bind(_effect)
+    const runner: any = _effect.run.bind(_effect)
+    // 方便stop
+    runner.effect = _effect
 
     return runner
+}
+
+
+export function stop(runner) {
+    runner.effect.stop()
 }
