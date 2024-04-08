6de936d11b689e119fd5e2e97aabbbf3fe51a40b
frylstar <frylstar@gmail.com>
2024-04-08 21:39:30 +0800

    feat: 解析三种联合类型

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -9,34 +9,63 @@ const enum TagType {
 export function baseParse(content: string) {
     const context = createParseContext(content);
 
-    return createRoot(parseChildren(context));
+    return createRoot(parseChildren(context, []));
 }
 
-function parseChildren(context) {
+function parseChildren(context, ancestors) {
     const nodes: any = [];
 
-    let node;
-    const s = context.source;
-    if (s.startsWith("{{")) {
-        node = parseInterpolation(context);
-    } else if (s[0] === "<") {
-        if (/[a-z]/i.test(s[1])) {
-            node = parseElement(context);
+    while (!isEnd(context, ancestors)) {
+        let node;
+        const s = context.source;
+        if (s.startsWith("{{")) {
+            node = parseInterpolation(context);
+        } else if (s[0] === "<") {
+            if (/[a-z]/i.test(s[1])) {
+                node = parseElement(context, ancestors);
+            }
         }
+    
+        if (!node) {
+            node = parseText(context);
+        }
+    
+        nodes.push(node);
     }
-
-    if (!node) {
-        node = parseText(context);
-    }
-
-    nodes.push(node);
 
     return nodes;
 }
 
+function isEnd(context, ancestors) {
+    // 2. 遇到结束标签的时候
+    const s = context.source;
+    if (s.startsWith("</")) {
+        for (let i = ancestors.length - 1; i >= 0; i--) {
+            const tag = ancestors[i].tag;
+            if (startsWithEndTagOpen(s, tag)) {
+                return true;
+            }
+        }
+    }
+    
+    // 1. source有值的时候，即不为“”，为“”则结束
+    return !s;
+}
+
 function parseText(context) {
+    let endIndex = context.source.length;
+    let endTokens = ["<", "{{"];
+
+    for (let i = 0; i < endTokens.length; i++) {
+        const index = context.source.indexOf(endTokens[i]);
+        if (index !== -1 && endIndex > index) {
+            endIndex = index;
+        }
+    }
+
     // 1. 获取content
-    const content = parseTextData(context, context.source.length);
+    const content = parseTextData(context, endIndex);
+    console.log('parseText结果: ', content)
 
     return {
         type: NodeTypes.TEXT,
@@ -52,21 +81,35 @@ function parseTextData(context: any, length) {
     return content;
 }
 
-function parseElement(context) {
+function parseElement(context, ancestors) {
     // Implement
-    const element = parseTag(context, TagType.Start);
+    const element: any = parseTag(context, TagType.Start);
 
-    parseTag(context, TagType.End);
+    ancestors.push(element);
 
-    console.log("------------", context);
+    element.children = parseChildren(context, ancestors);
+
+    ancestors.pop();
+
+    if (startsWithEndTagOpen(context.source, element.tag)) {
+        parseTag(context, TagType.End);
+    } else {
+        throw new Error(`缺少结束标签：${element.tag}`)
+    }
+
+    console.log("parseElement之后: ", context);
 
     return element;
+}
+
+function startsWithEndTagOpen(source, tag) {
+    return source.startsWith("</") && source.slice(2, 2 + tag.length).toLowerCase() === tag.toLowerCase()
 }
 
 function parseTag(context, type) {
     // 1. 解析tag(开始结束标签)
     const match: any = /^<\/?([a-z]*)/i.exec(context.source);
-    console.log(match); // [ '<div', 'div', index: 0, input: '<div></div>', groups: undefined ]
+    console.log('parseTag正则match结果:', match); // [ '<div', 'div', index: 0, input: '<div></div>', groups: undefined ]
     const tag = match[1];
     // 2. 删除处理完成的代码
     advanceBy(context, match[0].length);
@@ -81,6 +124,7 @@ function parseTag(context, type) {
     };
 }
 
+// 解析插值
 function parseInterpolation(context) {
     // {{ message }}
     const openDelimiter = "{{";
@@ -101,13 +145,13 @@ function parseInterpolation(context) {
     const rawContent = parseTextData(context, rawContentLength);
     const content = rawContent.trim(); // 去除空格
 
-    console.log("content:", content);
+    console.log("parseInterpolation结果1:", content);
 
     // 清空已处理的，继续推进
     // context.source = context.source.slice(closeDelimiter.length)
     advanceBy(context, closeDelimiter.length);
 
-    console.log("context.source:", context.source);
+    console.log("context.parseInterpolation结果2:", context.source);
 
     return {
         type: NodeTypes.INTERPOLATION, // 插值类型
