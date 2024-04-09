72d710fc436fadebfd7fbb5ee1d83176887c3c49
frylstar <frylstar@gmail.com>
2024-04-09 18:24:55 +0800

    feat: 实现代码生成插值类型

--- src/compiler-core/src/codegen.ts
+++ src/compiler-core/src/codegen.ts
@@ -1,37 +1,88 @@
+import { NodeTypes } from "./ast";
+import { TO_DISPLAY_STRING, helperMapName } from "./runtimeHelpers";
+
 export function generate(ast) {
-    const context = createCodegenContext()
+    const context = createCodegenContext();
     const { push } = context;
 
-    push('return ')
+    genFunctionPremble(ast, context); // 前导码
 
     const functionName = "render";
     const args = ["_ctx", "_cache"];
     const signature = args.join(", ");
 
-    push(`function ${functionName}(${signature}){`)
-    push('return ')
+    push(`function ${functionName}(${signature}){`);
+    push("return ");
 
-    genNode(ast.codegenNode, context)
-    push("}")
+    genNode(ast.codegenNode, context);
+    push("}");
 
-    console.log(context.code, 'final code')
+    console.log(context.code, "final code");
     return {
-        code: context.code
-    }
+        code: context.code,
+    };
 }
 
 function createCodegenContext() {
     const context = {
-        code: '',
+        code: "",
         push(source) {
             context.code += source;
-        }
-    }
+        },
+        helper(key) {
+            return `_${helperMapName[key]}`;
+        },
+    };
 
     return context;
 }
 
+function genFunctionPremble(ast, context) {
+    const { push } = context;
+    const VueBinging = "Vue";
+    const aliasHelper = (s) => `${helperMapName[s]}: _${helperMapName[s]}`;
+    if (ast.helpers.length > 0) {
+        push(
+            `const { ${ast.helpers
+                .map(aliasHelper)
+                .join(", ")} } = ${VueBinging}`
+        );
+    }
+    push("\n");
+    push("return ");
+}
+
 function genNode(node: any, context) {
+    switch (node.type) {
+        case NodeTypes.TEXT:
+            genText(node, context);
+            break;
+        case NodeTypes.INTERPOLATION:
+            genInterpolation(node, context)
+            break;
+        case NodeTypes.SIMPLE_EXPRESSION:
+            genExpression(node, context)
+            break;
+        default:
+            break;
+    }
+}
+
+function genExpression(node: any, context: any) {
+    const { push } = context;
+
+    push(`${node.content}`)
+}
+
+function genInterpolation(node: any, context: any) {
+    const { push, helper } = context;
+    console.log(node)
+    push(`${helper(TO_DISPLAY_STRING)}(`);
+    genNode(node.content, context)
+    push(')')
+}
+
+function genText(node: any, context: any) {
     const { push } = context;
 
     push(`'${node.content}'`);
--- src/compiler-core/src/transform.ts
+++ src/compiler-core/src/transform.ts
@@ -1,3 +1,6 @@
+import { NodeTypes } from "./ast";
+import { TO_DISPLAY_STRING } from "./runtimeHelpers";
+
 export function transform(root, options = {}) {
     const context = createTransformContext(root, options);
 
@@ -6,6 +9,8 @@ export function transform(root, options = {}) {
     traverseNode(root, context);
 
     createRootCodegen(root);
+
+    root.helpers = [...context.helpers.keys()];
 }
 
 function createRootCodegen(root) {
@@ -21,16 +26,24 @@ function traverseNode(node: any, context) {
         transform(node);
     }
 
-    traverseChildren(node, context);
+    switch (node.type) {
+        case NodeTypes.INTERPOLATION:
+            context.helper(TO_DISPLAY_STRING);
+            break;
+        case NodeTypes.ROOT:
+        case NodeTypes.ELEMENT:
+            traverseChildren(node, context);
+            break;
+        default:
+            break;
+    }
 }
 
 function traverseChildren(node, context) {
     const children = node.children;
-    if (children) {
-        for (let i = 0; i < children.length; i++) {
-            const node = children[i];
-            traverseNode(node, context);
-        }
+    for (let i = 0; i < children.length; i++) {
+        const node = children[i];
+        traverseNode(node, context);
     }
 }
 
@@ -38,6 +51,10 @@ function createTransformContext(root: any, options: any) {
     const context = {
         root,
         nodeTransforms: options.nodeTransforms || [],
+        helpers: new Map(),
+        helper(key) {
+            context.helpers.set(key, 1);
+        },
     };
 
     return context;
